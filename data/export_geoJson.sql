fa3a8773586c703b75e24049a4bc7daa4e7558e2
Michael Byrne <feomike@gmail.com>
2013-02-25 21:18:27 -0500

    adding data files

---
+++ data/export_geoJson.sql
@@ -0,0 +1,50 @@
+﻿--format for geoJSON
+--mike byrne
+--need to refine this a little
+--make sure it works; there are some issues on the editing style
+--------------------------------
+--------------------------------
+--first lines
+--{
+--"type": "FeatureCollection",
+--"features": [
+--------------------------------
+--------------------------------
+--for each line {"type": "Feature", "id": <GID>,"properties": { },
+--------------------------------
+--------------------------------
+--   ', "geometry": { "type": "Point", "Coordinates": [' || st_x(geom) || ', ' || st_y(geom) || '] }}, '
+--------------------------------
+--------------------------------
+--last lines
+--]
+--}
+--------------------------------
+--------------------------------
+
+copy 
+( select '{"type": "Feature", "id": '|| gid || ', "properties": {"gid": ' || gid || 
+    ', "total": ' || total || ', "teachers_schools": ' || teachers_schools ||
+    ', "work_study_jobs": ' || work_study_jobs || ', "head_start": ' || head_start ||
+    ', "cleanair_water": ' || cleanair_water || ', "military_readiness": ' || military_readiness ||
+    ', "law_enforcement": ' || law_enforcement || ', "job_search_assistance":' || job_search_assistance ||
+    ', "child_care": ' || child_care || ', "vaccines_children": ' || vaccines_children ||
+    ', "public_health": ' || public_health || ', "stop_violence_against_women": ' || stop_violence_against_women ||
+    ', "nutrition_assistance": ' || nutrition_assistance || '} ' ||
+    ', "geometry": { "type": "Point", "Coordinates": [' || st_x(geom) || ', ' || st_y(geom) || '] }}, '
+  from analysis.state_seq, carto.state_centroids
+  where state_seq.state=state_centroids.name_1
+  order by name_1 )
+  to '/Users/feomike/downloads/state_seq.geojson'
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ data/load-seq.sql
@@ -0,0 +1,32 @@
+﻿
+-- Table: analysis.state_seq
+
+DROP TABLE if exists analysis.state_seq;
+
+CREATE TABLE analysis.state_seq
+(
+  fips character varying(2),
+  State character varying(200),
+  Total numeric,
+  teachers_schools numeric,
+  work_study_jobs numeric,
+  head_start numeric,
+  cleanAir_water numeric,
+  military_readiness numeric,
+  law_enforcement numeric,
+  job_search_assistance numeric,
+  child_care numeric,
+  vaccines_children numeric,
+  public_health numeric,
+  stop_violence_against_women numeric,
+  nutrition_assistance numeric  
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE analysis.state_seq OWNER TO postgres;
+
+truncate analysis.state_seq; 
+copy analysis.state_seq
+  from '/Users/feomike/downloads/WH-State-Sequester-2.txt'
+  header csv delimiter '|';
